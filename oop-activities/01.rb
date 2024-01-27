881eaee00d44f533bfe63588d28cc0bc3bc77bb1
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-01-27 09:30:26 +0800

    01

--- oop-activities/01.rb
+++ oop-activities/01.rb
@@ -3,7 +3,10 @@
 def find_smallest_int(arr)
   smallest_int = arr[0]
   arr.each do |x|
-    if (smallest_int > x) ? smallest_int = x : smallest_int; end
+    smallest_int = (smallest_int > x) ? x : smallest_int
+
+    # if (smallest_int > x) ? smallest_int = x : smallest_int; end
+    
     # if smallest_int > x
     #   smallest_int = x
     # end
